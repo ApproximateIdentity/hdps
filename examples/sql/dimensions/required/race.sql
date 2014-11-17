3eda5e9319ab434bef99f8dfe2bc9eceeb77e78b
Thomas Nyberg <nyberg@ohdsi.org>
2014-11-17 10:41:19 -0500

    Add race dimension sql dimension code

---
+++ examples/sql/dimensions/required/race.sql
@@ -0,0 +1,20 @@
+/* Build race dimension table. Note that in the MSLR_CDM4 and CCAE_CDM4
+ * databases, the race information is not available. This means that every
+ * person is assigned 0 as a covariate value. However, this will not affect the
+ * analysis because this variable will have 0 predictive power.
+ */
+CREATE TABLE #dim (
+    person_id bigint,
+    covariate_id bigint,
+    covariate_count int
+);
+
+INSERT INTO #dim
+SELECT DISTINCT
+    cp.person_id,
+    p.race_concept_id AS covariate_id,
+    1 AS covariate_count
+FROM
+    #cohort_person cp INNER JOIN mslr_cdm4.person p
+        ON cp.person_id = p.person_id
+;
