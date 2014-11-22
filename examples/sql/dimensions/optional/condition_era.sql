6a5fa062d90c0f5661f918645dad5e6c8c35ad2a
Thomas Nyberg <nyberg@ohdsi.org>
2014-11-21 21:03:39 -0500

    Add ICD-9 condition_era sql code
    The previous condition_era code considered OMOP condition_concept_id
    values, but this is more granular for the HDPS algorithm (as well as not
    being hierarchical). The current code instead considers the equivalent
    ICD-9 codes and groups on the first 3 digits. Currently the 3 digit
    grouping is hard-coded, but that could presumably be changed to a
    parameter.

--- examples/sql/dimensions/optional/condition_era.sql
+++ examples/sql/dimensions/optional/condition_era.sql
@@ -1,16 +1,23 @@
 /* Build condition era dimension table. */
 INSERT INTO #dim
-SELECT DISTINCT
+SELECT
     cp.person_id,
-    ce.condition_concept_id as covariate_id,
-    COUNT(ce.condition_concept_id) as covariate_count
+    SUBSTRING(sm.source_code FROM 1 FOR 3) AS covariate_id,
+    COUNT(SUBSTRING(sm.source_code FROM 1 FOR 3)) AS covariate_count
+    /*COUNT(covariate_id) AS covariate_count*/
 FROM
-    #cohort_person cp INNER JOIN mslr_cdm4.condition_era ce
+    #cohort_person cp INNER JOIN @cdm_schema.dbo.condition_era ce
         ON cp.person_id = ce.person_id
+    INNER JOIN vocabulary.source_to_concept_map sm
+        ON ce.condition_concept_id = sm.target_concept_id
+    INNER JOIN vocabulary.vocabulary v
+        ON sm.source_vocabulary_id = v.vocabulary_id
 WHERE
     cp.cohort_start_date < ce.condition_era_start_date
     AND ce.condition_era_start_date <= cp.cohort_end_date
+    AND v.vocabulary_name ~* 'ICD-9'
+    AND SUBSTRING(sm.source_code FROM 1 FOR 3) ~ '^[0-9]{3}$'
 GROUP BY
     cp.person_id,
-    ce.condition_concept_id
+    covariate_id
 ;
