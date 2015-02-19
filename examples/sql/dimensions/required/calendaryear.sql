37db492d1414ed9f9dcfa3cbdf4e4fcf1c7f571d
Thomas Nyberg <nyberg@ohdsi.org>
2015-02-19 15:02:52 -0500

    Remove stale examples

--- examples/sql/dimensions/required/calendaryear.sql
+++
@@ -1,8 +0,0 @@
-/* Build 'calendar year' dimension table. */
-INSERT INTO #dim
-SELECT DISTINCT
-    cp.person_id,
-    YEAR(cp.cohort_start_date) AS covariate_id,
-    1 AS covariate_count
-FROM #cohort_person cp
-;
